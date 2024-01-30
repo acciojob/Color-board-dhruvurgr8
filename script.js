f2bf02721d3c410be77df98fbce9df5ff576d300
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-01-30 17:56:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.querySelector('.container');
+  const colors = [
+    '#3498db',
+    '#e74c3c',
+    '#2ecc71',
+    '#f39c12',
+    '#9b59b6',
+    '#1abc9c',
+   
+  ];
+
+  
+  for (let i = 0; i < 800; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+   
+    square.addEventListener('mouseover', () => {
+      
+      square.style.backgroundColor = getRandomColor();
+    });
+
+   
+    square.addEventListener('mouseout', () => {
+    
+      setTimeout(() => {
+        square.style.backgroundColor = '#3498db';
+      }, 1000);
+    });
+
+    container.appendChild(square);
+  }
+
+
+  function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+});
